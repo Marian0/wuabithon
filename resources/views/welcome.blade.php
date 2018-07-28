1989c9b2bb54962a1d826e77ed1fd3e68e970598
marian0 <marianosantafe@gmail.com>
2018-07-28 13:05:53 -0300

    Prototype form login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,66 +4,70 @@
     <div class="wizard-container">
         <div class="card wizard-card" data-color="red" id="wizard">
 
-            <div class="wizard-header">
-                <h3 class="wizard-title">
-                    Bienvenido a Wuabi
-                </h3>
-            </div>
+            <form action="./dashboard" method="get">
 
-            <div class="tab-content">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <h4 class="info-text">
-                            Ingrese su usuario y contraseña para continuar. Si no tenés cuenta podés
-                            crearla
-                            <a href="">haciendo click aquí</a>
-                        </h4>
-                    </div>
-                    <div class="col-sm-6 col-sm-offset-3">
+                <div class="wizard-header">
+                    <h3 class="wizard-title">
+                        Bienvenido a Wuabi
+                    </h3>
+                </div>
 
-                        <div class="input-group">
-                        <span class="input-group-addon">
-                            <i class="material-icons">email</i>
-                        </span>
-                            <div class="form-group label-floating">
-                                <label class="control-label">Your Email</label>
-                                <input name="name" type="text" class="form-control">
-                            </div>
+                <div class="tab-content">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h4 class="info-text">
+                                Ingrese su usuario y contraseña para continuar. Si no tenés cuenta podés
+                                crearla
+                                <a href="">haciendo click aquí</a>
+                            </h4>
                         </div>
 
-                        <div class="input-group">
-                    <span class="input-group-addon">
-                        <i class="material-icons">lock_outline</i>
-                    </span>
+                        <div class="col-sm-6 col-sm-offset-3">
 
-                            <div class="form-group label-floating">
-                                <label class="control-label">Your Password</label>
-                                <input name="name2" type="password" class="form-control">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                <i class="material-icons">email</i>
+                                </span>
+                                <div class="form-group label-floating">
+                                    <label class="control-label">Your Email</label>
+                                    <input name="email" type="email" autocomplete="off" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                <i class="material-icons">lock_outline</i>
+                                </span>
+
+                                <div class="form-group label-floating">
+                                    <label class="control-label">Your Password</label>
+                                    <input name="password" type="password" class="form-control">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="wizard-footer">
-                <div class="pull-right">
+                <div class="wizard-footer">
+                    <div class="pull-right">
 
-                    <a class="btn btn-danger btn-wd" href="./dashboard">
-                        <i class="material-icons">code</i>
-                        Ingresar
-                    </a>
+                        <button type="submit" class="btn btn-danger btn-wd" href="./dashboard">
+                            <i class="material-icons">code</i>
+                            Ingresar
+                        </a>
 
+                    </div>
+                    <div class="pull-left">
+                        <a class="btn btn-fill btn-wd" href="./register">
+                            <i class="material-icons">code</i>
+                            Crear cuenta
+                        </a>
+
+                    </div>
+                    <div class="clearfix"></div>
                 </div>
-                <div class="pull-left">
-                    <a class="btn btn-fill btn-wd" href="./register">
-                        <i class="material-icons">code</i>
-                        Crear cuenta
-                    </a>
 
-                </div>
-                <div class="clearfix"></div>
-            </div>
-
+            </form>
 
         </div>
     </div>
@@ -71,7 +75,7 @@
 
 @section('scripts')
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
 
         });
     </script>
