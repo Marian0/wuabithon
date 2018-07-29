19734c3fdb0db29b67d308252ac9b353b93bdcf3
marian0 <marianosantafe@gmail.com>
2018-07-29 11:13:04 -0300

    Highlight navbar based on current route

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,27 +6,34 @@
 
 <?php
 
+        $array = Route::current()->parameters();
+        $section = array_get($array, 'section');
+
 $menu = [
 
     [
         'icon' => 'list',
         'label' => 'Mis Pólizas',
-        'route' => route('dashboard', ['section' => 'list'])
+        'route' => route('dashboard', ['section' => 'list']),
+        'active' => $section == 'list'
     ],
     [
         'icon' => 'add',
         'label' => 'Crear Póliza',
-        'route' => route('dashboard', ['section' => 'create'])
+        'route' => route('dashboard', ['section' => 'create']),
+        'active' => $section == 'create'
     ],
     [
         'icon' => 'account_box',
         'label' => 'Perfil Scoring',
-        'route' => route('dashboard', ['section' => 'declare'])
+        'route' => route('dashboard', ['section' => 'declare']),
+        'active' => $section == 'declare'
     ],
     [
         'icon' => 'settings',
         'label' => 'Configuración',
-        'route' => route('dashboard', ['section' => 'settings'])
+        'route' => route('dashboard', ['section' => 'settings']),
+        'active' => $section == 'settings'
     ],
     [
         'icon' => 'exit_to_app',
@@ -44,7 +51,6 @@ $menu = [
     <div class="wizard-container dashboard-container">
         <div class="card wizard-card" data-color="red" id="wizard">
 
-
             <div class="wizard-header">
                 <h3 class="wizard-title">
                     Sistema de Coberturas
@@ -56,7 +62,7 @@ $menu = [
                 <ul>
                     <?php foreach($menu as $menu): ?>
                     <li>
-                        <a href="<?= array_get($menu, 'route') ?>">
+                        <a href="<?= array_get($menu, 'route') ?>" class="<?= array_get($menu, 'active') ? 'active': '' ?>">
                             <i class="material-icons"><?= array_get($menu, 'icon') ?></i>
                             <?= array_get($menu, 'label') ?></a>
                     </li>
